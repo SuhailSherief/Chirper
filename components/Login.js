f9b1fe371154363b0819efff39c2ea0fdbedf80f
SuhailSherief <suhailscreed@gmail.com>
2021-12-19 15:52:49 +0530

    login

--- components/Login.js
+++ components/Login.js
@@ -14,12 +14,11 @@ function Login({ providers }) {
       <div>
         {Object.values(providers).map((provider) => (
           <div key={provider.name}>
-            {/* https://devdojo.com/tailwindcss/buttons#_ */}
-            <button className="relative px-6 py-3 font-bold text-white rounded-lg group" onClick={()=> signIn(provider.id, {callbackUrl:'/'})}>
-    <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
-    <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-pink-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
-    <span className="relative">Sign in with {provider.name}</span>
-</button>
+            <button className="relative px-6 py-3 font-bold text-white rounded-lg group" onClick={() => signIn(provider.id, { callbackUrl: '/' })}>
+              <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-pink-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
+              <span className="relative">Sign in with {provider.name}</span>
+            </button>
           </div>
         ))}
       </div>
