3917e3046c89f2dd6cc2a24625c0bb3bdc886a02
SuhailSherief <suhailscreed@gmail.com>
2021-12-19 15:51:28 +0530

    feeds

--- components/Feed.js
+++ components/Feed.js
@@ -8,21 +8,21 @@ import Post from "./Post";
 function Feed() {
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => 
-        onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")),(snapshot) => {
+    useEffect(() =>
+        onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")), (snapshot) => {
             setPosts(snapshot.docs);
-        }),[db]);  
+        }), [db]);
     return (
-        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px] text-white">
+        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
             <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
-            <h2 className="text-lg sm:text-xl font-bold">Home</h2>
-            <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
-                <SparklesIcon className="h-5 text-white" />
-            </div>
+                <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+                <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
+                    <SparklesIcon className="h-5 text-white" />
+                </div>
             </div>
             <Input />
             <div className="pb-72">
-            {posts.map(post => (<Post key={post.id} id={post.id} post={post.data()} />))}
+                {posts.map(post => (<Post key={post.id} id={post.id} post={post.data()} />))}
             </div>
         </div>
     )
